f831adb12eeea304ac891b80e3a7a6717fae89d5
LokmanSaleh <lookman_sh@hotmail.com>
2023-01-14 14:47:38 -0500

    SaveObjectIntoXMLFile ReadObjectFromXMlFile EMF

---
+++ ProjectTemplateML/src/main/java/com/utiles/CreateSaveTester.java
@@ -0,0 +1,69 @@
+package com.utiles;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import projetTemplate.Database;
+import projetTemplate.MLProject;
+import projetTemplate.ProjetTemplateFactory;
+import projetTemplate.ProjetTemplatePackage;
+
+public class CreateSaveTester {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Initialize the model
+    	ProjetTemplatePackage.eINSTANCE.eClass();
+        // Retrieve the default factory singleton
+    	ProjetTemplateFactory factory = ProjetTemplateFactory.eINSTANCE;
+
+        // create the content of the model via this program
+        MLProject myWeb = factory.createMLProject();
+        Database page = factory.createDatabase();
+        page.setCredentias("credential");
+        page.setValue("LE valeur"); 
+        myWeb.setDatabase(page);
+        
+        // As of here we preparing to save the model content
+
+        // Register the XMI resource factory for the .website extension
+
+        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m = reg.getExtensionToFactoryMap();
+        m.put("projettemplate", new XMIResourceFactoryImpl());
+
+        // Obtain a new resource set
+        ResourceSet resSet = new ResourceSetImpl();
+
+        // create a resource
+//        Resource resource = resSet.createResource(URI
+//                .createURI("http://www.example.org/projetTemplate"));
+        Resource resource = resSet.createResource(URI
+                .createFileURI(new File("C:\\Users\\lookm\\git\\ProjetTemplateRep\\ProjectTemplateML\\src\\main\\java\\com\\ProjectTemplateML\\MLTemplate2.projettemplate").getAbsolutePath()));
+  
+        
+        // Get the first model element and cast it to the right type, in my
+        // example everything is hierarchical included in this first node
+        resource.getContents().add(myWeb);
+        resource.getContents().add(page);
+
+        // now save the content.
+        try {
+            resource.save(null);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
